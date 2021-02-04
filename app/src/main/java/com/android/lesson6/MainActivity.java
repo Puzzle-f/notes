a9b7b73a8cec765adb6168e8c61011d45572932f
[Puzzle] <[puzzleuri@mail.ru]>
2021-02-04 22:26:04 +0300

    добавлен экшн бар, но не доделана строка поиска

--- app/src/main/java/com/android/lesson6/MainActivity.java
+++ app/src/main/java/com/android/lesson6/MainActivity.java
@@ -1,11 +1,14 @@
 package com.android.lesson6;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -21,6 +24,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initButton();
+        initToolbar();
+    }
+
+    // инициилизируем сам тулбар
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+//    инициилизируем (вешаем слушателя) на кнопки тулбара
+//    пока не разобрадся
+
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        int id = item.getItemId();
+//        switch (id){
+//            case R.id.action_settings:
+//                addFragment(new FragmentCreatingNote());
+//                return true;
+//            case R.id.action_main:
+//
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    //    инициилизация меню поиска в тулбаре
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        инициилизируем кнопки тулбара
+        getMenuInflater().inflate(R.menu.main, menu);
+//        находим строку поиска и вешаем на неё слушателя
+        MenuItem search = menu.findItem(R.id.action_search);
+
+//        приложение падает на следующей строке
+
+//        SearchView searchText = (SearchView) search.getActionView();
+//        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+//                return true;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                return true;
+//            }
+//        });
+        return true;
     }
 
     public void initButton() {
@@ -36,9 +88,12 @@ public class MainActivity extends AppCompatActivity {
 //                    .add(R.id.cities, fragment).commitAllowingStateLoss();
                 fragmentTransaction.add(R.id.cities, fragmentCreatingNote);
                 buttonAddIsChecked = true;
+                buttonAddNote.setText("Ok");
             } else {
                 Toast toast = Toast.makeText(this, "сохранение заметки", Toast.LENGTH_LONG);
                 toast.show();
+                buttonAddIsChecked = false;
+                buttonAddNote.setText("+");
                 TextInputEditText textInputEditText = findViewById(R.id.edit_text_x3);
                 fragmentTransaction.remove(fragmentCreatingNote);
             }
